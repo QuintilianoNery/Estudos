c799854575048445fddfbbc78870a92cda5d8ced
QuintilianoNery <quintiliano@tecsystem.com.br>
2020-01-10 18:41:40 -0300

    Aula 04 - Looping

--- C#/Loops_04/Loops/ConsoleApp1/Program.cs
+++ C#/Loops_04/Loops/ConsoleApp1/Program.cs
@@ -3,10 +3,71 @@
 namespace ConsoleApp1
 {
     class Program
-    {
+    {    
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            //int a = 0;
+            //Sintax While
+            /*while (a < 5)
+            {
+                if (a == 1)
+                {
+                    a++;
+                    //Continue pula uma operação do loop
+                    continue;
+                }
+                Console.WriteLine(a);
+                a++;
+                
+
+
+                if (a == 1)
+                {
+                    break;
+                }
+                //break;
+                //c++;
+                //c=c=1;
+            }*/
+
+            //Sintax dowhile
+            /*do
+            {
+                Console.WriteLine(a);
+                a++;
+            } while (a < 3);
+            */
+
+            //Sintax do for
+            /*
+            for (int b = 0; b < 3; b++)
+            {
+                Console.WriteLine(b);
+            }
+            */
+
+            /*
+            foreach(int v in a)
+            {
+                Console.WriteLine(v);
+
+            }
+            */
+
+            //Sintaxe função recursiva 
+            /*
+            public int looping(int a)
+            {
+                Console.WriteLine(a);
+                if (a < 3)
+                {
+                    looping(a + 1);
+                }
+            }
+            */
+
         }
     }
 }
+
